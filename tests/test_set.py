f4f3569062613b35c452554d0c1940ce78785be5
agathemmanuel <agathemmanuel00@gmail.com>
2022-07-10 18:16:31 +0530

    Adding test set endpoint

---
+++ tests/test_set.py
@@ -0,0 +1,39 @@
+from fastapi.testclient import TestClient
+import requests
+
+from app.kvstore import app
+
+client = TestClient(app) 
+
+def test_set_correct_format():
+    """
+    To test the response status code
+    when provided with well formated input
+    """
+    res = client.post("/set",json={"key": "1","value": "1"})
+    print(res.json())
+    assert res.status_code == 200
+
+
+def test_set_bad_format():
+    """
+    To test the response status code
+    when provided with bad format
+    """
+    res = client.post("/set",json={"key":1})
+    print(res.json())
+    assert res.status_code == 422
+
+
+def test_set_with_data_in_db():
+    """
+    To test the response status code
+    when provided with existing key
+    """
+    res = client.post("/set",json={"key": "test-key","value": "test-value"})
+    print(res.json())
+    value = client.get("/get/test-key")
+    expected_output = "test-value"
+    output = value.json()
+    assert output == expected_output
+
